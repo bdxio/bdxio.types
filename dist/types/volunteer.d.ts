0e207f3249b49c3ee20818f8004aa8d60141f5e4
HubM <hubmoncenis@gmail.com>
2023-03-13 22:34:09 +0100

    🏷️ add volunteer type

--- dist/types/volunteer.d.ts
+++ dist/types/volunteer.d.ts
@@ -1,3 +1,20 @@
 export type Volunteer = {
     id: string;
+    firstname: string;
+    lastname: string;
+    active: boolean;
+    job: {
+        label: string;
+        company: {
+            name?: string;
+            url?: string;
+        };
+    };
+    socials: {
+        github?: string;
+        twitter?: string;
+        linkedin?: string;
+        website?: string;
+    };
+    picture?: string;
 };
